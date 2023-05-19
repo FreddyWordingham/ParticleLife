1497c2cc0c8a02eac96b49428dbd11ee1c7fcb93
FreddyWordingham <freddy@digilab.co.uk>
2023-05-19 01:22:32 +0100

    Added bevy application

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use bevy::{prelude::*, window::close_on_esc};
+use rand::prelude::*;
+
+// == Settings ==
+
+// == Main ==
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(SimulationPlugin)
+        .run();
+}
+
+// == Plugins ==
+struct SimulationPlugin;
+impl Plugin for SimulationPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_system(close_on_esc);
+    }
 }
