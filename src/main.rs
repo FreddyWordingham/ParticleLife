01d76741f92ff58e993219648f06c4052d373bed
FreddyWordingham <freddy@digilab.co.uk>
2023-05-19 19:41:11 +0100

    Added attraction matrix resource

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,16 @@ use rand::prelude::*;
 const INIT_WINDOW_WIDTH: f32 = 1600.0;
 const INIT_WINDOW_HEIGHT: f32 = 1200.0;
 
+const TOTAL_SPECIES: usize = 6;
+
+// const TOTAL_PARTICLES: usize = 1000;
+// const PARTICLE_SIZE: f32 = 5.0;
+// const PARTICLE_MASS: f32 = 6.0;
+
+// const FRICTION_HALF_LIFE: f32 = 0.04;
+// // const friction_coefficient: f32 = 2.0f32.powf(-delta_time / FRICTION_HALF_LIFE);
+// const R_MAX: f32 = 100.0;
+
 // == Main ==
 fn main() {
     App::new()
@@ -27,8 +37,27 @@ fn main() {
 struct SimulationPlugin;
 impl Plugin for SimulationPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(spawn_camera)
+        app.init_resource::<AttractionMatrix>()
+            .add_startup_system(spawn_camera)
             .add_system(close_on_esc);
+    }
+}
+
+// == Resources ==
+#[derive(Resource)]
+struct AttractionMatrix([[f32; TOTAL_SPECIES]; TOTAL_SPECIES]);
+impl Default for AttractionMatrix {
+    fn default() -> Self {
+        let mut coefficients = [[0.0; TOTAL_SPECIES]; TOTAL_SPECIES];
+        for i in 0..TOTAL_SPECIES {
+            for j in 0..TOTAL_SPECIES {
+                coefficients[i][j] = (random::<f32>() * 2.0) - 1.0;
+                print!("{:.2} ", coefficients[i][j]);
+            }
+            println!("");
+        }
+
+        Self(coefficients)
     }
 }
 
