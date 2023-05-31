d4a29cfc7212f770af5829c9ba207868719e5f40
FreddyWordingham <freddy@digilab.co.uk>
2023-05-31 19:29:10 +0100

    Removed silly function

--- src/settings.rs
+++ src/settings.rs
@@ -4,12 +4,12 @@ pub const INIT_WINDOW_WIDTH: f32 = 1000.0;
 pub const INIT_WINDOW_HEIGHT: f32 = 1000.0;
 pub const WINDOW_CLEAR_COLOUR: Color = Color::rgb(0.1, 0.1, 0.1);
 
-pub const TOTAL_SPECIES: usize = 7;
+pub const TOTAL_SPECIES: usize = 9;
 
-pub const TOTAL_PARTICLES: usize = 100;
-pub const PARTICLE_RADIUS: f32 = 2.0;
-pub const PARTICLE_MASS: f32 = 1.0;
+pub const TOTAL_PARTICLES: usize = 1000;
+pub const PARTICLE_RADIUS: f32 = 1.4;
+pub const PARTICLE_MASS: f32 = 0.1;
 
 pub const FRICTION_HALF_LIFE: f32 = 0.04;
-pub const R_MAX: f32 = 100.0;
+pub const R_MAX: f32 = 50.0;
 pub const BETA: f32 = 0.3;
