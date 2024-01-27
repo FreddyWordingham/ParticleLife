29fb90b2f017db9f3b7f64ff7d871a5703fd6970
Freddy Wordingham <freddy@digilab.co.uk>
2024-01-27 20:58:37 +0000

    Removed unused module

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 mod components;
-mod events;
 mod plugins;
 mod resources;
 mod settings;
@@ -7,7 +6,6 @@ mod systems;
 mod utilities;
 
 pub use components::*;
-pub use events::*;
 pub use plugins::*;
 pub use resources::*;
 pub use settings::*;
